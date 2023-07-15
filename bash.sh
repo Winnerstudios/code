e79d0f13a10d03db2a091fbd5fe53486c3ac8621
Winnerstudios <112678939+Winnerstudios@users.noreply.github.com>
2023-07-15 16:57:09 -0500

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+echo "Loading..whats your name"
+read name
+echo "welocme $name"
+
+echo" please wait"
+
+sudo apt update
+echo "are you ready"
+read
+echo "okay oh well"
+cd pho
+ls
+<html>
+<h1>hello</html>
+</html>
